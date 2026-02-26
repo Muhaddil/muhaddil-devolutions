8566ba35e211a13c32572c7a2b8efd0412f58faf
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2026-02-26 20:18:48 +0100

    feat: Enable automatic version checking on resource start

--- config.lua
+++ config.lua
@@ -2,6 +2,7 @@ Config = {}
 
 Config.FrameWork = "auto" -- auto, esx, qb
 Config.ESXVer = "new" -- new, old
+Config.AutoVersionChecker = true -- Automatically checks for updates on resource start
 Config.AllowedGroups = {
     qb = { "admin", "god" },         -- QBCore roles
     esx = { "admin", "superadmin" }, -- ESX groups
